40decea485221e0d7c3c2231f274ad69b628b0a3
Trine <trinem95@hotmail.com>
2018-12-03 15:10:51 +0100

    ny tekst

--- intro.js
+++ intro.js
@@ -1,8 +1,8 @@
 // Viser butlerens svar til kongen
 function nesteButlerBoble() {
-    document.querySelector(".snakkeboble-konge").classList.add = ":active"
+    document.querySelector(".snakkeboble-konge").style.animation = "fadeout 1s linear";
     document.querySelector(".snakkeboble-konge").style.display = "none";
-    document.querySelector(".snakkeboble-butler").classList.add = ":active"
+    document.querySelector(".snakkeboble-konge").style.animation = "fadeout 1s linear";
     document.querySelector(".snakkeboble-butler").style.display = "block";
 }
 
@@ -10,4 +10,5 @@ function nesteButlerBoble() {
 function nesteKongeBoble() {
     document.querySelector(".snakkeboble-butler").style.display = "none";
     document.querySelector(".snakkeboble-konge").style.display = "block";
+    document.querySelector("#konge-tekst-1").innerHTML = "Som ny konge trenger jeg informasjon fra de andre landene.";
 }
