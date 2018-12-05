64c61651bc9f1edc090862fd9f5d52d61abce0b7
Andreas Hjørnevik <andreas.hjornevik@gmail.com>
2018-12-05 11:46:56 +0100

    navn oppdatering og små fiks

--- quiz.js
+++ quiz.js
@@ -1,18 +1,18 @@
 function rigthAnswer() {
-    document.getElementById("rigth").style.borderColor = "green";
+    document.getElementById("rigth").style.borderColor = "limegreen";
 }
 
 function barcaAnswer() {
     document.getElementById("wrong").style.borderColor = "red";
-    document.getElementById("rigth").style.borderColor = "green";
+    document.getElementById("rigth").style.borderColor = "limegreen";
 }
 
 function grandAnswer() {
     document.getElementById("wrong1").style.borderColor = "red";
-    document.getElementById("rigth").style.borderColor = "green";
+    document.getElementById("rigth").style.borderColor = "limegreen";
 }
 
 function bronAnswer() {
     document.getElementById("wrong2").style.borderColor = "red";
-    document.getElementById("rigth").style.borderColor = "green";
+    document.getElementById("rigth").style.borderColor = "limegreen";
 }
